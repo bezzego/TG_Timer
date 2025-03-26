7c28af06ec1af4406551de96c25c70fdda22b042
Egor Bezborodov <egor.bezborodoff@gmail.com>
2025-03-26 12:13:54 +0300

    Добавил def main

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import os
+from typing import Optional
 from pytimeparse import parse
 from dotenv import load_dotenv
 import ptbot
 import progressbar
 
+load_dotenv()
+TG_TOKEN = os.getenv('TG_TOKEN')
+
+bot: Optional[ptbot.Bot] = None
+
 
 def wait(chat_id, question):
     seconds = parse(question)
-    message_id = bot.send_message(chat_id, f"Осталось: {seconds} секунд\n{progressbar.render_progressbar(seconds, seconds)}")
-    bot.create_countdown(seconds, notify_progress, chat_id=chat_id, message_id=message_id, total=seconds)
+    message_id = bot.send_message(
+        chat_id,
+        f"Осталось: {seconds} секунд\n{progressbar.render_progressbar(seconds, seconds)}"
+    )
+    bot.create_countdown(
+        seconds,
+        notify_progress,
+        chat_id=chat_id,
+        message_id=message_id,
+        total=seconds
+    )
     bot.create_timer(seconds, reply, chat_id=chat_id)
     print("Мне написал пользователь с ID:", chat_id)
     print("Пользователь попросил поставить таймер на:", question)
@@ -23,9 +38,12 @@ def reply(chat_id):
     bot.send_message(chat_id, "Время вышло!")
 
 
-if __name__ == '__main__':
-    load_dotenv()
-    TG_TOKEN = os.getenv('TG_TOKEN')
+def main():
+    global bot
     bot = ptbot.Bot(TG_TOKEN)
     bot.reply_on_message(wait)
     bot.run_bot()
+
+
+if __name__ == '__main__':
+    main()
