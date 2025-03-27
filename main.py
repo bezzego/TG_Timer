abbcbf347b2cb918f1764e95c8bb36e76f61e5ec
Egor Bezborodov <egor.bezborodoff@gmail.com>
2025-03-27 13:27:46 +0300

    все убрал в main

--- main.py
+++ main.py
@@ -1,47 +1,58 @@
 import os
-from typing import Optional
-from pytimeparse import parse
 from dotenv import load_dotenv
+from pytimeparse import parse
+from functools import partial
+
 import ptbot
 import progressbar
 
 
-bot: Optional[ptbot.Bot] = None
-
-
-def wait(chat_id, question):
+def wait(chat_id, question, bot):
     seconds = parse(question)
     message_id = bot.send_message(
         chat_id,
-        f"Осталось: {seconds} секунд\n{progressbar.render_progressbar(seconds, seconds)}"
+        f"Осталось: {seconds} секунд\n"
+        f"{progressbar.render_progressbar(seconds, seconds)}"
     )
+
     bot.create_countdown(
         seconds,
-        notify_progress,
-        chat_id=chat_id,
-        message_id=message_id,
-        total=seconds
+        partial(
+            notify_progress,
+            chat_id=chat_id,
+            message_id=message_id,
+            total=seconds,
+            bot=bot
+        )
     )
-    bot.create_timer(seconds, reply, chat_id=chat_id)
+
+    bot.create_timer(
+        seconds,
+        partial(reply, chat_id=chat_id, bot=bot)
+    )
+
     print("Мне написал пользователь с ID:", chat_id)
     print("Пользователь попросил поставить таймер на:", question)
 
 
-def notify_progress(secs_left, chat_id, message_id, total):
+def notify_progress(secs_left, chat_id, message_id, total, bot):
     bar = progressbar.render_progressbar(total, secs_left)
-    bot.update_message(chat_id, message_id, f"Осталось: {secs_left} секунд\n{bar}")
+    bot.update_message(
+        chat_id,
+        message_id,
+        f"Осталось: {secs_left} секунд\n{bar}"
+    )
 
 
-def reply(chat_id):
+def reply(chat_id, bot):
     bot.send_message(chat_id, "Время вышло!")
 
 
 def main():
-    global bot
     load_dotenv()
-    TG_TOKEN = os.getenv('TG_TOKEN')
-    bot = ptbot.Bot(TG_TOKEN)
-    bot.reply_on_message(wait)
+    tg_token = os.getenv('TG_TOKEN')
+    bot = ptbot.Bot(tg_token)
+    bot.reply_on_message(partial(wait, bot=bot))
     bot.run_bot()
 
 
